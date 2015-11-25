e3e2376ec75d8a276a2c20e4c0beeabedbc101ea
phf <phf@atm-consulting.fr>
2015-11-25 09:14:25 +0100

    UPDATE logo config

--- admin/remisetotal_setup.php
+++ admin/remisetotal_setup.php
@@ -83,7 +83,7 @@ llxHeader('', $langs->trans($page_name));
 // Subheader
 $linkback = '<a href="' . DOL_URL_ROOT . '/admin/modules.php">'
     . $langs->trans("BackToModuleList") . '</a>';
-print_fiche_titre($langs->trans($page_name), $linkback);
+print_fiche_titre($langs->trans($page_name), $linkback, 'title_setup.png');
 
 // Configuration header
 $head = remisetotalAdminPrepareHead();
